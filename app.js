07c0f48d57ae700105b204ef41acc28aad44b154
benber7 <90457184+benber7@users.noreply.github.com>
2023-04-18 09:51:22 +0200

    ting
    Co-authored-by: HuviXYZ <HuviXYZ@users.noreply.github.com>
    Co-authored-by: mnu0plays <mnu0plays@users.noreply.github.com>
    Co-authored-by: Breaking Å <e-aarhol@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+//const dbDyr = require("better-sqlite3")("data.db"); //{verbose: console.log}
+const hbs = require("hbs");
+const path = require("path");
+
+const app = express();     
+
+app.use(session({
+    secret: "detteherersjult",
+    resave: false,
+    saveUninitialized: false // Cookies settes ikke før endringer har blitt gjort på nettsiden
+})) 
+
+app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.urlencoded({extended: true}))
+app.set("view engine", hbs);
+app.set("views", path.join(__dirname, "./views/pages"))
+
+
+// Dette er hovedsiden, du søker bare på http://localhost:3000/ og får opp en html side
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"))
+})
+
+//Disse to er veier til registrer og login siden, de er koblet til hver sin knapp i navbaren på de fleste sidene
+app.get("/Public/registrer", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Public/registrer.html"))
+})
+app.get("/Public/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Public/login.html"))
+})
+
+// Koden som kjører når du trykker på login 
+app.post("/login", async (req, res) => {
+    let login = req.body;
+    // Henter ut data fra brukere.db, user
+    let userData = dbFolk.prepare("SELECT * FROM user WHERE email = ?").get(login.email);
+    
+    // Her bruker jeg await for å gi bcrypt.compare nok til til å sammen ligne passordet du skrev inn med hashen som ligger i databasen
+    if(await bcrypt.compare(login.password, userData.hash)) {
+        // hvis passordet du skrev in er lik hashen blir du redirected til index.html og hvis de er ikke lik blir du redirected tilbake
+        req.session.loggedin = true
+        res.redirect("/")
+    } else {
+        res.redirect("back")
+    }
+})
+
+// Når du tyrkker på logg ut kjører den her. Den gjør om din cookie session fra true til false og redirecter deg til index.html
+app.get("/logut", (req, res) => {
+    req.session.loggedin = false;
+    res.redirect("/");
+});
+
+// Etter du har skrevet inn dine opplysninger og trykker på registrer
+app.post(("/addUser"), async (req, res) => {
+    // henter ut hva du har skrevet
+    let svar = req.body;
+    // omgjør passordet du skrev inn til hash
+    let hash = await bcrypt.hash(svar.password, 10)
+    //console.log(svar)
+    //console.log(hash)
+    // sender alt til database og der etter sender deg tilbake
+    dbFolk.prepare("INSERT INTO user (name, email, hash) VALUES (?, ?, ?)").run(svar.name, svar.email, hash)
+    res.redirect("back")    
+})
+
+app.listen("3000", () => {
+    console.log("Server listening at http://localhost:3000")
+})
